14eca16f904f23f610aca0efdb116c7dd23e82ba
Jack <jmcgirl3@gmail.com>
2024-02-21 17:32:58 +0100

    Added new DeepCopy method Updated RotOranges() if statements to use the value of that element as previously checked, not the element of its value in the array Fixed check to discover if right hand column to being col-1 instead of hardcoded 4 Small order of operation fix

--- RottenOranges/Program.cs
+++ RottenOranges/Program.cs
@@ -17,11 +17,12 @@ internal class Program
 {
     static int rows = 4;
     static int cols = 6;
-    static int[] todaysOranges = new int[] { 0, 1, 2, 0, 0, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 1, 1,1 ,1,2,1,1,1,1 };
+    static int[] todaysOranges = new int[] { 0, 1, 1, 1, 1, 2, 0, 1, 1, 0, 1, 2, 0, 1, 2, 0, 1, 1, 1 , 1, 1, 1, 1, 2 };
     static int[] tomorrowsOranges = new int[todaysOranges.Length];
     static int daysPassed = 0;
     private static void Main(string[] args)
     {
+        Console.WriteLine("Starting Crate...");
         PrintCrate(rows, cols, todaysOranges);
         MainLoop();
     }
@@ -29,9 +30,9 @@ internal class Program
     private static void MainLoop()
       {
         EdgeChecker(rows, cols, todaysOranges);
-        if (todaysOranges == tomorrowsOranges)
+        
+        if (todaysOranges.SequenceEqual(tomorrowsOranges))
         {
-            PrintCrate(rows, cols, todaysOranges);
             if (todaysOranges.Contains(1))
             {
                 PrintAnswer(-1);
@@ -43,21 +44,18 @@ internal class Program
                 return;
             }
         }
-        todaysOranges = tomorrowsOranges;
-        tomorrowsOranges.ToList().Clear();
-        PrintCrate(rows, cols, todaysOranges);
         daysPassed++;
+        Console.WriteLine($"Crate after Day {daysPassed}");
+        todaysOranges = DeepCopy(tomorrowsOranges);
+        PrintCrate(rows, cols, todaysOranges);
         MainLoop();
     }
 
     private static void EdgeChecker(int rows, int cols, int[] oranges)
     {
+        tomorrowsOranges = DeepCopy(oranges);
         for (int i = 0; i < oranges.Length; i++) 
         {
-            if (oranges[i] != 0)
-            {
-                tomorrowsOranges[i] = oranges[i];
-            }
             Edge edge = new Edge();
             edge = Edge.NotEdge;
             //Edge Represents different sides/corners
@@ -75,7 +73,7 @@ internal class Program
                 {
                     edge = Edge.TopLeft;
                 }
-                if (i == oranges.Length-cols)
+                else if (i == oranges.Length-cols)
                 {
                     edge = Edge.BottomLeft;
                 }
@@ -84,7 +82,7 @@ internal class Program
                     edge = Edge.Left;
                 }
             }
-            if (i % cols == 4)
+            if (i % cols == cols-1)
             {
                 if (i == cols-1)
                 {
@@ -105,8 +103,7 @@ internal class Program
 
     private static void RotOranges(int rows, int cols, int[] oranges, Edge edge, int currentSlot)
     {
-        
-        if (oranges[currentSlot] == 2)
+         if (oranges[currentSlot] == 2)
         {
             if (edge == Edge.NotEdge)
             {
@@ -117,17 +114,16 @@ internal class Program
                 if (slotLeft == 1)
                 {
                     tomorrowsOranges[currentSlot - 1] = 2;
-                    todaysOranges[currentSlot] =4; 
                 }
-                if (oranges[slotRight] == 1)
+                if (slotRight == 1)
                 {
                     tomorrowsOranges[currentSlot + 1] = 2;
                 }
-                if (oranges[slotBelow] == 1)
+                if (slotBelow == 1)
                 {
                     tomorrowsOranges[currentSlot + cols] = 2;
                 }
-                if (oranges[slotAbove] == 1)
+                if (slotAbove == 1)
                 {
                     tomorrowsOranges[currentSlot - cols] = 2;
                 }
@@ -136,11 +132,11 @@ internal class Program
             {
                 var slotRight = oranges[currentSlot + 1];
                 var slotBelow = oranges[currentSlot + cols];
-                if (oranges[slotRight] == 1)
+                if (slotRight == 1)
                 {
                     tomorrowsOranges[currentSlot + 1] = 2;
                 }
-                if (oranges[slotBelow] == 1)
+                if (slotBelow == 1)
                 {
                     tomorrowsOranges[currentSlot + cols] = 2;
                 }
@@ -154,11 +150,11 @@ internal class Program
                 {
                     tomorrowsOranges[currentSlot-1] = 2;
                 }
-                if (oranges[slotRight] == 1)
+                if (slotRight == 1)
                 {
                     tomorrowsOranges[currentSlot+1] = 2;
                 }
-                if (oranges[slotBelow] == 1)
+                if (slotBelow == 1)
                 {
                     tomorrowsOranges[currentSlot + cols] = 2;
                 }
@@ -171,7 +167,7 @@ internal class Program
                 {
                     tomorrowsOranges[currentSlot - 1] = 2;
                 }
-                if (oranges[slotBelow] == 1)
+                if (slotBelow == 1)
                 {
                     tomorrowsOranges[currentSlot + cols] = 2;
                 }
@@ -185,11 +181,11 @@ internal class Program
                 {
                     tomorrowsOranges[currentSlot - 1] = 2;
                 }
-                if (oranges[slotBelow] == 1)
+                if (slotBelow == 1)
                 {
                     tomorrowsOranges[currentSlot + cols] = 2;
                 }
-                if (oranges[slotAbove] == 1)
+                if (slotAbove == 1)
                 {
                     tomorrowsOranges[currentSlot - cols] = 2;
                 }
@@ -202,7 +198,7 @@ internal class Program
                 {
                     tomorrowsOranges[currentSlot - 1] = 2;
                 }
-                if (oranges[slotAbove] == 1)
+                if (slotAbove == 1)
                 {
                     tomorrowsOranges[currentSlot - cols] = 2;
                 }
@@ -216,11 +212,11 @@ internal class Program
                 {
                     tomorrowsOranges[currentSlot - 1] = 2;
                 }
-                if (oranges[slotRight] == 1)
+                if (slotRight == 1)
                 {
                     tomorrowsOranges[currentSlot + 1] = 2;
                 }
-                if (oranges[slotAbove] == 1)
+                if (slotAbove == 1)
                 {
                     tomorrowsOranges[currentSlot - cols] = 2;
                 }
@@ -229,11 +225,11 @@ internal class Program
             {
                 var slotRight = oranges[currentSlot + 1];
                 var slotAbove = oranges[currentSlot - cols];
-                if (oranges[slotRight] == 1)
+                if (slotRight == 1)
                 {
                     tomorrowsOranges[currentSlot + 1] = 2;
                 }
-                if (oranges[slotAbove] == 1)
+                if (slotAbove == 1)
                 {
                     tomorrowsOranges[currentSlot - cols] = 2;
                 }
@@ -243,21 +239,20 @@ internal class Program
                 var slotRight = oranges[currentSlot + 1];
                 var slotAbove = oranges[currentSlot - cols];
                 var slotBelow = oranges[currentSlot + cols];
-                if (oranges[slotRight] == 1)
+                if (slotRight == 1)
                 {
                     tomorrowsOranges[currentSlot + 1] = 2;
                 }
-                if (oranges[slotBelow] == 1)
+                if (slotBelow == 1)
                 {
                     tomorrowsOranges[currentSlot + cols] = 2;
                 }
-                if (oranges[slotAbove] == 1)
+                if (slotAbove == 1)
                 {
                     tomorrowsOranges[currentSlot - cols] = 2;
                 }
             }
         }
-        
     }
 
     public static void PrintCrate(int rows, int columns, int[] oranges)
@@ -292,5 +287,11 @@ internal class Program
             result = $"It took {numberOfDays} days to rot all the oranges";
         }
         Console.WriteLine(result);
+    }
+    public static int[] DeepCopy(int[] array)
+    {
+        int[] copy = new int[array.Length];
+        Array.Copy(array, copy, array.Length);
+        return copy;
     }
 }
